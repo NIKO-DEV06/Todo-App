a3a3f7b6e9874d243063d92ba248f0fc27b5c8ed
Ayeniko Emmanuel <ayenikoemmanuel06@gmail.com>
2023-02-22 10:39:44 +0000

    added drag and drop

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,59 +1,87 @@
 import React, { useState } from "react";
 
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
 import ListItem from "./ListItem";
 
 const List = (props) => {
+  const [todoDrag, updateTodoDrag] = useState(props.todos);
+
+  const handleOnDragEnd = (result) => {
+    if (!result.destination) return;
+
+    const items = Array.from(todoDrag);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+
+    updateTodoDrag(items);
+  };
+
   const deleteTodoHandler = (id) => {
     props.newTodos(props.todos.filter((todo) => todo.id !== id));
   };
-
   return (
     <div className="flex justify-center">
-      <ul className="w-[85%] bg-white dark:bg-veryDarkDesaturatedBlue rounded-md mt-3 md:w-1/2 lg:w-[40%] ">
-        <div>
-          {props.todos.length === 0 ? (
-            <div className=" text-center p-3 border-b-2 dark:border-veryDarkGrayishBlue text-lg text-veryDarkGrayishBlue dark:bg-veryDarkDesaturatedBlue dark:text-veryLightGrayishBlue rounded-t-md">
-              There are no todos.🧍
-            </div>
-          ) : (
-            ""
-          )}
-          {props.todos.map((todo) => (
-            <ListItem
-              key={todo.id}
-              completed={todo.completed}
-              todoItem={todo.todo}
-              onCheck={() => {
-                const newTodos = [...props.todos];
-                const index = newTodos.findIndex((t) => t.id === todo.id);
-                newTodos[index].completed = !newTodos[index].completed;
-                props.newTodos(newTodos);
-              }}
-              onDeleteTodo={() => {
-                deleteTodoHandler(todo.id);
-              }}
-            />
-          ))}
-        </div>
+      <DragDropContext onDragEnd={handleOnDragEnd}>
+        <Droppable droppableId="todos">
+          {(provided) => {
+            return (
+              <ul
+                className="w-[85%] bg-white dark:bg-veryDarkDesaturatedBlue rounded-md mt-3 md:w-1/2 lg:w-[40%]"
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+              >
+                {props.todos.length === 0 ? (
+                  <div className=" text-center p-3 border-b-2 dark:border-veryDarkGrayishBlue text-lg text-veryDarkGrayishBlue dark:bg-veryDarkDesaturatedBlue dark:text-veryLightGrayishBlue rounded-t-md">
+                    There are no todos.🧍
+                  </div>
+                ) : null}
+                {todoDrag.map((todo, index) => {
+                  return (
+                    <ListItem
+                      key={todo.id}
+                      completed={todo.completed}
+                      todoItem={todo.todo}
+                      onId={todo.id}
+                      onIndex={index}
+                      onCheck={() => {
+                        const newTodos = [...props.todos];
+                        const index = newTodos.findIndex(
+                          (t) => t.id === todo.id
+                        );
+                        newTodos[index].completed = !newTodos[index].completed;
+                        props.newTodos(newTodos);
+                      }}
+                      onDeleteTodo={() => {
+                        deleteTodoHandler(todo.id);
+                      }}
+                    />
+                  );
+                })}
+                {provided.placeholder}
 
-        <div>
-          <li className="p-4 w-full flex justify-between ">
-            <span className="ml-4 text-darkGrayishBlue font-semibold">
-              {props.todos.filter((todo) => !todo.completed).length} items left
-            </span>
-            <button
-              onClick={() => {
-                const newTodos = [...props.todos];
-                const filtered = newTodos.filter((t) => !t.completed);
-                props.newTodos(filtered);
-              }}
-              className="text-darkGrayishBlue font-semibold hover:text-veryDarkGrayishBlue duration-200 dark:hover:text-veryLightGrayishBlue"
-            >
-              Clear Completed
-            </button>
-          </li>
-        </div>
-      </ul>
+                <div>
+                  <li className="p-4 w-full flex justify-between ">
+                    <span className="ml-4 text-darkGrayishBlue font-semibold">
+                      {props.todos.filter((todo) => !todo.completed).length}{" "}
+                      items left
+                    </span>
+                    <button
+                      onClick={() => {
+                        const newTodos = [...props.todos];
+                        const filtered = newTodos.filter((t) => !t.completed);
+                        props.newTodos(filtered);
+                      }}
+                      className="text-darkGrayishBlue font-semibold hover:text-veryDarkGrayishBlue duration-200 dark:hover:text-veryLightGrayishBlue"
+                    >
+                      Clear Completed
+                    </button>
+                  </li>
+                </div>
+              </ul>
+            );
+          }}
+        </Droppable>
+      </DragDropContext>
     </div>
   );
 };
--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,23 +1,33 @@
 import React from "react";
 
+import { Draggable } from "react-beautiful-dnd";
 import deleteBtn from "../assets/icon-cross.svg";
 
 const ListItem = (props) => {
   return (
-    <li className="p-4 border-b-2 border-veryLightGrayishBlue dark:border-veryDarkGrayishBlue w-full flex dark:bg-veryDarkDesaturatedBlue">
-      <input
-        checked={props.completed}
-        onChange={props.onCheck}
-        type="checkbox"
-        className="appearance-none bg-white dark:bg-veryDarkDesaturatedBlue border-[1.5px] border-veryLightGrayishBlue dark:border-veryDarkGrayishBlue h-6 w-6 rounded-full cursor-pointer my-auto checked:border-none  checked:bg-gradient-to-b from-from to-to peer relative after:content-[''] after:w-full after:h-full after:absolute after:left-0 after:top-0 after:bg-no-repeat after:bg-center after:bg-[length:12px] after:bg-[url('.././src/assets/icon-check.svg')] after:opacity-0 after:checked:opacity-100 hover:"
-      />
-      <span className="flex-1 ml-4 text-veryDarkGrayishBlue peer-checked:text-lightGrayishBlue dark:peer-checked:text-darkGrayishBlue peer-checked:line-through duration-200 dark:text-veryLightGrayishBlue">
-        {props.todoItem}
-      </span>
-      <button className="flex" onClick={props.onDeleteTodo}>
-        <img src={deleteBtn} alt="" className="w-4 h-4 my-auto" />
-      </button>
-    </li>
+    <Draggable key={props.onId} draggableId={props.onId} index={props.onIndex}>
+      {(provided) => (
+        <li
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          className="p-4 border-b-2 border-veryLightGrayishBlue dark:border-veryDarkGrayishBlue w-full flex dark:bg-veryDarkDesaturatedBlue"
+        >
+          <input
+            checked={props.completed}
+            onChange={props.onCheck}
+            type="checkbox"
+            className="appearance-none bg-white dark:bg-veryDarkDesaturatedBlue border-[1.5px] border-veryLightGrayishBlue dark:border-veryDarkGrayishBlue h-6 w-6 rounded-full cursor-pointer my-auto checked:border-none  checked:bg-gradient-to-b from-from to-to peer relative after:content-[''] after:w-full after:h-full after:absolute after:left-0 after:top-0 after:bg-no-repeat after:bg-center after:bg-[length:12px] after:bg-[url('.././src/assets/icon-check.svg')] after:opacity-0 after:checked:opacity-100 hover:"
+          />
+          <span className="flex-1 ml-4 text-veryDarkGrayishBlue peer-checked:text-lightGrayishBlue dark:peer-checked:text-darkGrayishBlue peer-checked:line-through duration-200 dark:text-veryLightGrayishBlue">
+            {props.todoItem}
+          </span>
+          <button className="flex" onClick={props.onDeleteTodo}>
+            <img src={deleteBtn} alt="" className="w-4 h-4 my-auto" />
+          </button>
+        </li>
+      )}
+    </Draggable>
   );
 };
 
